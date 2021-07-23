a4eeb446b13ca1032f05c7f725324a804715be98
coderwhy <coderwhy@gmail.com>
2021-07-23 10:49:15 +0800

    项目配置

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+
+    
+  }
+})
 </script>
 
 <style lang="less">
