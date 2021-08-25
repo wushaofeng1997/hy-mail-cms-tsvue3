b74730edeb2f5f85ac311ab2944c0b1ed481d819
coderwhy <coderwhy@gmail.com>
2021-08-25 22:45:45 +0800

    fix(coderwhy): coderwhy

--- src/views/main/analysis/overview/config/project-dir.ts
+++ src/views/main/analysis/overview/config/project-dir.ts
@@ -1,160 +1,254 @@
 export const projectDir = `
-├── App.vue
-├── assets
-│   ├── css
-│   │   └── index.less
-│   └── img
-│       ├── login-bg.svg
-│       ├── login-bg1.jpg
-│       ├── login-bg2.jpg
-│       ├── login-bg3.jpg
-│       └── login-bg4.jpg
-├── base-ui
-│   ├── code
-│   │   ├── index.ts
-│   │   └── src
-│   │       └── index.vue
-│   ├── count-up
-│   │   ├── index.ts
-│   │   └── src
-│   │       └── index.vue
-│   ├── descriptions
-│   │   ├── index.ts
-│   │   └── src
-│   │       ├── data.d.ts
-│   │       └── index.vue
-│   ├── editor
-│   │   ├── index.ts
-│   │   └── src
-│   │       └── index.vue
-│   ├── menu-guide
-│   │   ├── index.ts
-│   │   └── src
-│   │       ├── data.d.ts
-│   │       └── index.vue
-│   └── text-link
-│       ├── index.ts
-│       └── src
-│           └── index.vue
-├── components
-│   ├── card
-│   │   ├── index.ts
-│   │   └── src
-│   │       └── index.vue
-│   ├── nav-header
-│   │   ├── index.ts
-│   │   └── src
-│   │       ├── index.vue
-│   │       └── user-info.vue
-│   ├── nav-menu
-│   │   ├── index.ts
-│   │   └── src
-│   │       └── index.vue
-│   └── statistical-panel
-│       ├── index.ts
-│       └── src
-│           └── index.vue
-├── config
-│   ├── config.dev.ts
-│   └── index.ts
-├── constants
-│   └── cache_keys.ts
-├── global
-│   ├── filters.ts
-│   └── index.ts
-├── hooks
-│   ├── useECharts.ts
-│   └── useEventListener.ts
-├── main.ts
-├── router
-│   ├── guard
-│   │   ├── index.ts
-│   │   └── permission-guard.ts
-│   └── index.ts
-├── service
-│   ├── index.ts
-│   ├── login
-│   │   └── index.ts
-│   ├── main
-│   │   ├── analysis
-│   │   ├── index.ts
-│   │   ├── product
-│   │   ├── story
-│   │   └── system
-│   │       ├── department
-│   │       │   ├── index.ts
-│   │       │   └── types.d.ts
-│   │       ├── menu
-│   │       │   ├── index.ts
-│   │       │   └── types.d.ts
-│   │       ├── role
-│   │       │   ├── index.ts
-│   │       │   └── types.d.ts
-│   │       └── user
-│   │           ├── index.ts
-│   │           └── types.d.ts
-│   ├── request.ts
-│   └── types.d.ts
-├── shims-vue.d.ts
-├── store
-│   ├── index.ts
-│   └── modules
-│       └── user.ts
-├── style
-│   └── _var.less
-├── test
-│   └── tsc
-│       ├── index.js
-│       └── index.ts
-├── utils
-│   ├── cache.ts
-│   ├── common.ts
-│   ├── date-format.ts
-│   └── menu.ts
-└── views
-    ├── login
-    │   ├── cpns
-    │   │   └── login-panel.vue
-    │   └── login.vue
-    └── main
-        ├── analysis
-        │   ├── dashboard
-        │   │   ├── cpns
-        │   │   │   ├── bar-echart.vue
-        │   │   │   ├── china-echart.vue
-        │   │   │   ├── china.json
-        │   │   │   ├── data.ts
-        │   │   │   ├── line-echart.vue
-        │   │   │   ├── mock-data.ts
-        │   │   │   ├── pie-echart.vue
-        │   │   │   └── rose-echart.vue
-        │   │   └── dashboard.vue
-        │   └── overview
-        │       ├── overview.vue
-        │       └── p-config
-        │           ├── dependencies.ts
-        │           ├── dev-dependencies.ts
-        │           ├── index.ts
-        │           ├── project-dir.ts
-        │           └── technology-stacks.ts
-        ├── main.vue
-        ├── product
-        │   ├── category
-        │   │   └── category.vue
-        │   └── goods
-        │       └── goods.vue
-        ├── story
-        │   ├── chat
-        │   │   └── chat.vue
-        │   └── list
-        │       └── list.vue
-        └── system
-            ├── department
-            │   └── department.vue
-            ├── menu
-            │   └── menu.vue
-            ├── role
-            │   └── role.vue
-            └── user
-                └── user.vue
+├── README.md
+├── babel.config.js
+├── commitlint.config.js
+├── package-lock.json
+├── package.json
+├── public
+│   ├── favicon.ico
+│   └── index.html
+├── src
+│   ├── App.vue
+│   ├── assets
+│   │   ├── css
+│   │   │   ├── base.css
+│   │   │   ├── index.css
+│   │   │   └── variable.less
+│   │   └── img
+│   │       ├── login-bg.svg
+│   │       └── logo.svg
+│   ├── base-ui
+│   │   ├── breadcrumb
+│   │   │   ├── index.ts
+│   │   │   ├── src
+│   │   │   │   └── breadcrumb.vue
+│   │   │   └── types
+│   │   │       └── index.ts
+│   │   ├── card
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── card.vue
+│   │   ├── code
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── code.vue
+│   │   ├── count-up
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── countup.vue
+│   │   ├── descriptions
+│   │   │   ├── index.ts
+│   │   │   ├── src
+│   │   │   │   └── descriptions.vue
+│   │   │   └── types
+│   │   │       └── types.ts
+│   │   ├── echarts
+│   │   │   ├── data
+│   │   │   │   └── china.json
+│   │   │   ├── hooks
+│   │   │   │   └── useEcharts.ts
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── base-echart.vue
+│   │   ├── form
+│   │   │   ├── index.ts
+│   │   │   ├── src
+│   │   │   │   └── form.vue
+│   │   │   └── types
+│   │   │       └── index.ts
+│   │   ├── table
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── table.vue
+│   │   └── text-link
+│   │       ├── index.ts
+│   │       └── src
+│   │           └── textlink.vue
+│   ├── components
+│   │   ├── nav-header
+│   │   │   ├── hooks
+│   │   │   │   └── useMenuIconHook.ts
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       ├── nav-header.vue
+│   │   │       └── nav-info.vue
+│   │   ├── nav-menu
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── nav-menu.vue
+│   │   ├── page-charts
+│   │   │   ├── index.ts
+│   │   │   ├── src
+│   │   │   │   ├── bar-echart.vue
+│   │   │   │   ├── line-echart.vue
+│   │   │   │   ├── map-echart.vue
+│   │   │   │   ├── pie-echart.vue
+│   │   │   │   └── rose-echart.vue
+│   │   │   ├── types
+│   │   │   │   └── index.ts
+│   │   │   └── utils
+│   │   │       ├── convert-data.ts
+│   │   │       └── coordinate-data.ts
+│   │   ├── page-content
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── page-content.vue
+│   │   ├── page-modal
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── page-modal.vue
+│   │   ├── page-search
+│   │   │   ├── index.ts
+│   │   │   └── src
+│   │   │       └── page-search.vue
+│   │   └── statistical-panel
+│   │       ├── index.ts
+│   │       └── src
+│   │           └── statistical-panel.vue
+│   ├── global
+│   │   ├── index.ts
+│   │   ├── register-element.ts
+│   │   └── register-properties.ts
+│   ├── hooks
+│   │   ├── useEcharts.ts
+│   │   ├── usePageModal.ts
+│   │   ├── usePageSearch.ts
+│   │   └── usePermission.ts
+│   ├── main.ts
+│   ├── router
+│   │   ├── index.ts
+│   │   ├── login
+│   │   │   └── login.ts
+│   │   ├── main
+│   │   │   ├── analysis
+│   │   │   │   ├── dashboard
+│   │   │   │   │   └── dashboard.ts
+│   │   │   │   └── overview
+│   │   │   │       └── overview.ts
+│   │   │   ├── main.ts
+│   │   │   ├── product
+│   │   │   │   ├── category
+│   │   │   │   │   └── category.ts
+│   │   │   │   └── goods
+│   │   │   │       └── goods.ts
+│   │   │   ├── story
+│   │   │   │   ├── chat
+│   │   │   │   │   └── chat.ts
+│   │   │   │   └── list
+│   │   │   │       └── list.ts
+│   │   │   └── system
+│   │   │       ├── department
+│   │   │       │   └── department.ts
+│   │   │       ├── menu
+│   │   │       │   └── menu.ts
+│   │   │       ├── role
+│   │   │       │   └── role.ts
+│   │   │       └── user
+│   │   │           └── user.ts
+│   │   └── not-found
+│   │       └── not-found.ts
+│   ├── service
+│   │   ├── index.ts
+│   │   ├── login
+│   │   │   ├── login.ts
+│   │   │   └── types.ts
+│   │   ├── main
+│   │   │   ├── analysis
+│   │   │   │   └── analysis.ts
+│   │   │   ├── product
+│   │   │   └── system
+│   │   │       └── system.ts
+│   │   └── request
+│   │       ├── config.ts
+│   │       ├── request.ts
+│   │       └── type.ts
+│   ├── shims-vue.d.ts
+│   ├── store
+│   │   ├── index.ts
+│   │   ├── login
+│   │   │   ├── login.ts
+│   │   │   └── types.ts
+│   │   ├── main
+│   │   │   ├── analysis
+│   │   │   │   ├── analysis.ts
+│   │   │   │   └── types.ts
+│   │   │   ├── product
+│   │   │   └── system
+│   │   │       ├── system.ts
+│   │   │       └── types.ts
+│   │   └── types.ts
+│   ├── utils
+│   │   ├── cache.ts
+│   │   ├── date-formt.ts
+│   │   └── map-menu.ts
+│   └── views
+│       ├── login
+│       │   ├── cpns
+│       │   │   ├── login-account.vue
+│       │   │   ├── login-account2.vue
+│       │   │   ├── login-panel.vue
+│       │   │   └── login-phone.vue
+│       │   ├── login.vue
+│       │   └── types
+│       │       └── index.ts
+│       ├── main
+│       │   ├── analysis
+│       │   │   ├── dashboard
+│       │   │   │   └── dashboard.vue
+│       │   │   └── overview
+│       │   │       ├── config
+│       │   │       │   ├── dependencies.ts
+│       │   │       │   ├── dev-dependencies.ts
+│       │   │       │   ├── index.ts
+│       │   │       │   ├── project-dir.ts
+│       │   │       │   └── technology-stacks.ts
+│       │   │       └── overview.vue
+│       │   ├── main.vue
+│       │   ├── product
+│       │   │   ├── category
+│       │   │   │   ├── category.vue
+│       │   │   │   └── config
+│       │   │   │       ├── content.config.ts
+│       │   │   │       └── search.config.ts
+│       │   │   └── goods
+│       │   │       ├── config
+│       │   │       │   ├── content.config.ts
+│       │   │       │   └── search.config.ts
+│       │   │       └── goods.vue
+│       │   ├── story
+│       │   │   ├── chat
+│       │   │   │   └── chat.vue
+│       │   │   └── list
+│       │   │       ├── config
+│       │   │       │   └── content.config.ts
+│       │   │       └── list.vue
+│       │   └── system
+│       │       ├── department
+│       │       │   ├── config
+│       │       │   │   ├── content.config.ts
+│       │       │   │   ├── modal.config.ts
+│       │       │   │   └── search.config.ts
+│       │       │   └── department.vue
+│       │       ├── menu
+│       │       │   ├── config
+│       │       │   │   └── content.config.ts
+│       │       │   └── menu.vue
+│       │       ├── role
+│       │       │   ├── config
+│       │       │   │   ├── content.config.ts
+│       │       │   │   ├── modal.config.ts
+│       │       │   │   └── search.config.ts
+│       │       │   └── role.vue
+│       │       └── user
+│       │           ├── config
+│       │           │   ├── content.config.ts
+│       │           │   ├── modal.config.ts
+│       │           │   └── search.config.ts
+│       │           └── user.vue
+│       └── not-found
+│           └── not-found.vue
+├── tsconfig.json
+├── types
+│   └── vuex.d.ts
+└── vue.config.js
 `
